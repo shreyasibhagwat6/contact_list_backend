9ceed76aeef498b75a3fbe0998b1adcfb4e6ea29
shreyasibhagwat6 <shreyasibhagwat6@gmail.com>
2024-05-07 23:09:34 -0400

    CRUD routes created

---
+++ prisma/migrations/20240508024952_init/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `contact` MODIFY `phoneNumber` VARCHAR(191) NOT NULL;
--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { PrismaClient } = require("@prisma/client")
+
+const prisma = new PrismaClient();
 
 app.use(cors());
 app.use(express.json());
 
 app.get('/', async (req, res) => {
-    res.send("hello");
+    const allContacts = await prisma.contact.findMany();
+    res.json(allContacts);
+})
+
+app.post('/', async (req, res) => {
+    const newContact = await prisma.contact.create({ data: req.body });
+    res.json(newContact);
+})
+
+app.put('/:id', async (req, res) => {
+    const id = req.params.id;
+    const newFirstName = req.body.firstName;
+    const updatedContact = await prisma.contact.update({ where: {id: parseInt(id)}, data: {firstName: newFirstName} });
+    res.json(updatedContact);
+})
+
+app.delete('/:id', async (req, res) => {
+    const id = req.params.id;
+    const deletedContact = await prisma.contact.delete({ where: {id: parseInt(id)} });
+    res.json(deletedContact);
 })
 
 app.listen(3001, () => console.log('hello'))
